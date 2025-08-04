98fd5d6ecbe2cf6a849df1db243e260f7a8f85a2
Shahbaz Bokhari <syedshahbazhaider007@gmail.com>
2025-08-04 11:15:41 +0100

    made things more minimal and efficient

--- embedded-system/include/server.hpp
+++ embedded-system/include/server.hpp
@@ -1,15 +1,1 @@
-#include <WebServer.h>
-#include <SdFat.h>
-
-extern WebServer server;
-extern SemaphoreHandle_t sdMutex;
-
-void setupServer();
-void handleRoot();
-void handleDays();
-void handleDrives();
-void handleDrive();
-void handleLiveData();
-void handleSDInfo();
-
 
